13a29aca5cacdca54fcf15679e92e0d140307f33
瑞 佟 <tongrui_tr@163.com>
2020-06-08 21:29:49 +0800

    0

--- mnistexample.py
+++ mnistexample.py
@@ -12,7 +12,36 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-#将模型的各层堆叠起来，以搭建 tf.keras.Sequential 模型。为训练选择优化器和损失函数：
+#fashion_mnist = keras.datasets.fashion_mnist
+#(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# let's display the first 25 images from the training set and display the class name below each image.
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+#将模型的各层堆叠起来，以搭建 tf.keras.Sequential 模型。为训练选择优化器和损失函数
+#The first layer in this network, tf.keras.layers.Flatten
+#transforms the format of the images from a two-dimensional array (of 28 by 28 pixels) 
+#to a one-dimensional array (of 28 * 28 = 784 pixels). 
+#Think of this layer as unstacking rows of pixels in the image and lining them up. 
+#This layer has no parameters to learn; it only reformats the data.
+#After the pixels are flattened, the network consists of a sequence of two tf.keras.layers.Dense layers.
+#These are densely connected, or fully connected, neural layers. 
+#The first Dense layer has 128 nodes (or neurons). The second (and last) layer returns a logits array with length of 10. 
+#Each node contains a score that indicates the current image belongs to one of the 10 classes.
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
@@ -27,6 +56,7 @@ model.fit(x_train, y_train, epochs=10)
 test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
 
 print('\nTest accuracy:', test_acc)
+
 
 
 
