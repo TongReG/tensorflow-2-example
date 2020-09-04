47efc5bdecffcd8b9c7de993b41df4c3bbc8ab96
瑞 佟 <tongrui_tr@163.com>
2020-09-04 20:55:00 +0800

    Update cocoexample.py

--- cocoexample.py
+++ cocoexample.py
@@ -10,8 +10,9 @@
 #http://images.cocodataset.org/annotations/image_info_test2017.zip
 import os
 import time
-import math, random
-from multiprocessing import Process, Queue, cpu_count
+import math
+import random
+from multiprocessing import Process, Queue, cpu_count, freeze_support
 import tensorflow as tf
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # 这一行注释掉可以调用GPU，不注释时使用CPU
 import numpy as np
@@ -35,32 +36,48 @@ path_instances = 'annotations/instances_train2017.json'
 path_captions = 'annotations/captions_train2017.json'
 #COCO训练集的图像数据保存的目录
 img_path = 'train2017/'
+catas_dict = {}
+imgrcg_box = {}
 
 #COCO_CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
 #                'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
 #                'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog',
-#                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe',
-#                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+#                'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra',
+#                'giraffe',
+#                'backpack', 'umbrella', 'handbag', 'tie', 'suitcase',
+#                'frisbee',
 #                'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat',
 #                'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-#                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+#                'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon',
+#                'bowl',
 #                'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot',
 #                'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',*
-#                'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop',
-#                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+#                'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+#                'laptop',
+#                'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+#                'oven',
 #                'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
 #                'scissors', 'teddy bear', 'hair drier', 'toothbrush')
 
-#COCO_LABEL_MAP = { 1:  1,  2:  2,  3:  3,  4:  4,  5:  5,  6:  6,  7:  7,  8:  8,
-#                   9:  9, 10: 10, 11: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16,
-#                  18: 17, 19: 18, 20: 19, 21: 20, 22: 21, 23: 22, 24: 23, 25: 24,
-#                  27: 25, 28: 26, 31: 27, 32: 28, 33: 29, 34: 30, 35: 31, 36: 32,
-#                  37: 33, 38: 34, 39: 35, 40: 36, 41: 37, 42: 38, 43: 39, 44: 40,
-#                  46: 41, 47: 42, 48: 43, 49: 44, 50: 45, 51: 46, 52: 47, 53: 48,
-#                  54: 49, 55: 50, 56: 51, 57: 52, 58: 53, 59: 54, 60: 55, 61: 56,
-#                  62: 57, 63: 58, 64: 59, 65: 60, 67: 61, 70: 62, 72: 63, 73: 64,
-#                  74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81: 72,
-#                  82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90: 80}
+#COCO_LABEL_MAP = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8,
+#                   9: 9, 10: 10, 11: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17:
+#                   16,
+#                  18: 17, 19: 18, 20: 19, 21: 20, 22: 21, 23: 22, 24: 23, 25:
+#                  24,
+#                  27: 25, 28: 26, 31: 27, 32: 28, 33: 29, 34: 30, 35: 31, 36:
+#                  32,
+#                  37: 33, 38: 34, 39: 35, 40: 36, 41: 37, 42: 38, 43: 39, 44:
+#                  40,
+#                  46: 41, 47: 42, 48: 43, 49: 44, 50: 45, 51: 46, 52: 47, 53:
+#                  48,
+#                  54: 49, 55: 50, 56: 51, 57: 52, 58: 53, 59: 54, 60: 55, 61:
+#                  56,
+#                  62: 57, 63: 58, 64: 59, 65: 60, 67: 61, 70: 62, 72: 63, 73:
+#                  64,
+#                  74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81:
+#                  72,
+#                  82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90:
+#                  80}
 
 #annotations字段是包含多个annotation实例的一个数组.
 #segmentation格式取决于这个实例
@@ -136,6 +153,7 @@ def gen_tfrecord(trainrecords, targetfolder, num, queue):
     for record in trainrecords:
         file_num += 1
         fields = record.split(',')
+        print('fields =>',fields)
         img = cv2.imread(fields[0] + fields[1])
         height, width, _ = img.shape
         img_jpg = cv2.imencode('.jpg', img)[1].tobytes()
@@ -246,18 +264,17 @@ if __name__ == '__main__':
     coco_train = pycocotools.coco.COCO(path_instances)
     COCO_CLASSES = coco_train.dataset['categories']
     train_ids = list(coco_train.imgToAnns.keys())
-    if len(self.ids) == 0:  # 如果没有标签或者不需要GT，则直接使用image
-        train_ids = list(coco_train.imgs.keys())
+    #if len(self.ids) == 0: # 如果没有标签或者不需要GT，则直接使用image
+    #    train_ids = list(coco_train.imgs.keys())
 
     # display COCO categories and supercategories
     catas = coco_train.loadCats(coco_train.getCatIds())
-    catas_dict = {}
     for cata in catas:
         catas_dict[cata['id']] = cata['name']
  
     #获取COCO数据集中所有图像的ID,构建训练集文件列表，里面的每个元素是路径名+图片文件名
     trainimg_ids = coco_train.getImgIds()
-    print(len(trainimg_ids))
+    print('trainimg_NUMBERS =>',len(trainimg_ids))
     train_images_filenames = os.listdir(img_path)
     #查找训练集的图片是否都有对应的ID，并保存到一个列表中
     trainimg_path = []
@@ -272,13 +289,12 @@ if __name__ == '__main__':
     random.shuffle(trainimg_path)
  
     all_catas = set()   #保存目标检测所有的类别，COCO共定义了90个类别，只有80个类别有检测数据
-    imgrcg_box = {}
     #获取每个图像的目标检测框的数据并保存
     for path in trainimg_path:
         boxes = [[],[],[],[],[]]
         fname = path.split(',')[1]
         imgid = int(fname[:-4])
-        annIds_nocrowd = coco_train.getAnnIds(imgIds=imgid, iscrowd=None)
+        annIds_nocrowd = coco_train.getAnnIds(imgIds=imgid, iscrowd=False)
         annoations = coco_train.loadAnns(annIds_nocrowd)
         for ann in annoations:
             bbox = ann['bbox']
@@ -293,6 +309,7 @@ if __name__ == '__main__':
             boxes[3].append(x_max)
             boxes[4].append(y_max)
         imgrcg_box[fname] = boxes
+        print('imgrcg_box',fname,'=>',imgrcg_box[fname])
  
     #获取有目标检测数据的80个类别的名称
     all_cata_list = list(all_catas)
@@ -301,14 +318,14 @@ if __name__ == '__main__':
         all_cata_dict[all_cata_list[i]] = i
     print(all_cata_dict)
 
-    cores = int(cpu_count()/2)   #定义用到的CPU处理的核心数
+    cores = int(cpu_count() / 2)   #定义用到的CPU处理的核心数
     max_num = 1536   #每个TFRECORD文件包含的最多的图像数
     print('\nCPU Nums =>',cores,'\n')
 
     if not os.path.exists('coco_record/'):
         os.makedirs('coco_record/')
 
-    print('Start processing train data using %i CPU cores:' % cores)
+    print('Start processing train data using %i CPU cores:' % cores,'/n')
     startime = time.time()        
     total_processed = process_in_queues(trainimg_path, cores, targetfolder='coco_record/')
     endtime = time.time()
@@ -344,7 +361,8 @@ if __name__ == '__main__':
                                                                classifier_activation='softmax')
     std_incv4 = tf.keras.applications.InceptionResNetV2(include_top=True, weights='imagenet', classifier_activation='softmax')
 
-    #img = tf.keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
+    #img = tf.keras.preprocessing.image.load_img(img_path, target_size=(224,
+    #224))
     #x = tf.keras.preprocessing.image.img_to_array(img)
     #x = np.expand_dims(x, axis=0)
     #x = tf.keras.applications.vgg16.preprocess_input(x)
@@ -370,7 +388,7 @@ if __name__ == '__main__':
     except StopIteration:
         print('\n遍历结束,迭代次数为',count,'\n')
  
-    #验证数据 
+    #验证数据
     imgdex = random.randint(0,count)     #select one image in the batch
     image = images_decode[imgdex]
     image_bbox = bbox_run[imgdex]
